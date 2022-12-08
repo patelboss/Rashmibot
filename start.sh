4296ce40d03134bc8baac1229c636471d6471636
patelboss <83952959+patelboss@users.noreply.github.com>
2022-12-08 17:36:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/patelboss/Rashmibot/tree/master.git /rashmi
+  git clone https://github.com/patelboss/Rashmibot /Rashmibot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /rashmi
+  git clone $UPSTREAM_REPO /Rashmibot
 fi
-cd /rashmi
+cd /Rashmibot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
